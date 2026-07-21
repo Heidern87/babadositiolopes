cec037a9eddb7d17ea15bb76374ae21679edea27
heidern87 <almirgalego43@gmail.com>
2026-07-21 16:20:00 -0300

    Conectando pasta ao GitHub

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'baba-sitio-lopes-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
