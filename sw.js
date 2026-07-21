578d8bcb6517537fbb1cfae73f8cbd694e4c3861
heidern87 <almirgalego43@gmail.com>
2026-07-21 17:24:54 -0300

    Ajustado cronometro flexivel com tempo digitado

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'baba-sitio-lopes-v2';
+const CACHE_NAME = 'baba-sitio-lopes-v2.1.1';
 const ASSETS = ['./', './index.html', './manifest.json', './baba_do_sitio_lopes.png'];
 
 self.addEventListener('install', (e) => {
